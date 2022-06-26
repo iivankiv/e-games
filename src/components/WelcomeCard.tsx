3e46b67744e0e2baa62b3c91fe61f182c3541e03
Petro Ivankiv <petro.ivankiv88@gmail.com>
2022-06-26 16:33:14 +0300

    style: updated styles

--- src/components/WelcomeCard.tsx
+++ src/components/WelcomeCard.tsx
@@ -5,9 +5,9 @@ type Props = {
 
 const WelcomeCard = ({ title = 'Welcome!t', text = 'React and Tailwind CSS in action' }: Props) => {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">{title}</p>
-      <p className="text-gray-500 text-lg">{text}</p>
+    <div className="container mx-auto bg-slate-500 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-white font-bold mb-5">{title}</p>
+      <p className="text-white text-lg">{text}</p>
     </div>
   );
 };
