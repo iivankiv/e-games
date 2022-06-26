2669c513282a5afebd6dd0c549557bf2a9399413
Petro Ivankiv <petro.ivankiv88@gmail.com>
2022-06-26 16:31:12 +0300

    style: updated styles

--- src/layouts/main-layout/header.component.tsx
+++ src/layouts/main-layout/header.component.tsx
@@ -7,8 +7,8 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className="flex justify-center items-center relative h-16 bg-slate-600 text-white">
-      <Link to="/" className="absolute left-4 rounded bg-blue-300 hover:bg-blue-400 py-2 px-4 cursor-pointer">
-        <FontAwesomeIcon icon={faHome} className="mr-3" /> Home
+      <Link to="/" className="absolute left-4 rounded bg-slate-500 hover:bg-slate-700 py-2 px-4 cursor-pointer">
+        <FontAwesomeIcon icon={faHome} />
       </Link>
       <div>Header!!!</div>
     </header>
